863a439fa3b161b3406a5d6244613527319400e5
overburn <overburn12@gmail.com>
2024-01-12 19:39:06 -0500

    added proper spacing

--- database.py
+++ database.py
@@ -55,6 +55,7 @@ image_tag = Table('image_tag', Base.metadata,
 #-----------------------------------------------------------------------
 # csv functions
 #-----------------------------------------------------------------------
+
 def parse_date(date_str):
     try:
         return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
